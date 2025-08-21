b1e407213b02d3e8769f2ab4e003096288c6c9db
adnanakhlasofficial <official.adnanakhlas@gmail.com>
2025-08-21 23:02:58 +0600

    add: 404 route

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,13 @@ app.use(express.json());
 
 app.use(routes);
 
+app.use((req, res, next) => {
+  res.status(404).json({
+    success: false,
+    message: `Route not found: ${req.originalUrl}`,
+  });
+});
+
 app.get("/", (req: Request, res: Response) => {
   res.send("Welcome to Library Management System.");
 });
